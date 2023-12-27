30264f152088a8f90366d03453f5c30a365ce957
RudrakshTaya <124330693+RudrakshTaya@users.noreply.github.com>
2023-12-27 12:47:13 +0530

    Add files via upload

---
+++ Rest and water notification Code.py
@@ -0,0 +1,29 @@
+import plyer
+from plyer import notification
+import time
+while True:
+    if __name__ == "__main__":
+        notification.notify(
+
+            title="TAKE REST",
+            message=" DESCRIPTION HERE",
+
+            ##app_icon=" \\Mac\Home\Downloads\icon.ico",
+
+            # displaying time
+            timeout=5
+        )
+
+
+        if __name__ == "__main__":
+            notification.notify(
+                title="DRINK WATER",
+                message=" DESCRIPTION HERE",
+
+                ##app_icon=" ",
+
+                # displaying time
+                timeout=5
+            )
+            # waiting time
+            time.sleep(60*60)
